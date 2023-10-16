6ef8c2f1e9bb1b309146a1977bba0b5e2944c96f
TyYipp <144461219+TyYipp@users.noreply.github.com>
2023-10-16 10:14:57 -0400

    10/16
    dom selectors video watching, nothing changed

--- dom.js
+++ dom.js
@@ -1,14 +1,25 @@
+// const DOMSelectors = {
+//     form: document.querySelector("#form"),
+//     firstName: document.querySelector(".first-name"),
+//     h2s: document.querySelectorAll("h2")
+//     //select the text box 
+//     //select all h2s in one property
+// };
+// // console.log(DOMSelectors.h2s);
+// DOMSelectors.form.addEventListener("submit", function (event) {
+//     event.preventDefault();
+//     console.log(DOMSelectors.firstName.value)
+//     DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.firstName.value
+//     );
+// });
 const DOMSelectors = {
-    form: document.querySelector("#form"),
-    firstName: document.querySelector(".first-name"),
-    h2s: document.querySelectorAll("h2")
-    //select the text box 
-    //select all h2s in one property
-};
-// console.log(DOMSelectors.h2s);
-DOMSelectors.form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    console.log(DOMSelectors.firstName.value)
-    DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.firstName.value
-    );
-});
+        form: document.querySelector("#form"),
+        firstName: document.querySelector(".first-name"),
+        h2s: document.querySelectorAll("h2")
+ 
+    };
+    function backgroundAndText(background, text) {
+        background.style.backgroundColor = "red"
+        text.innerHTML = "This is now a big red box"
+    }
+    backgroundAndText(DOMSelectors.box, DOMSelectors.text)
