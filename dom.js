cd18221493911d943bb490a0df454ffe114b4904
TyYipp <144461219+TyYipp@users.noreply.github.com>
2023-10-26 09:35:48 -0400

    10/26
    milestone

--- dom.js
+++ dom.js
@@ -1,13 +1,28 @@
-
 const DOMSelectors = {
-        form: document.querySelector("#form"),
-        firstName: document.querySelector(".first-name"),
-        h2s: document.querySelectorAll("h2")
+    button: document.getElementById("btn"),
+    box: document.getElementById("box"),
+    input1: document.getElementById('input1'),
+    input2: document.getElementById('input2'),
+  };
  
-    };
-    function backgroundAndText(background, text) {
-        background.style.backgroundColor = "red"
-        text.innerHTML = "This is now a big red box"
-    }
-    backgroundAndText(DOMSelectors.box, DOMSelectors.text)
-    DOMSelectors.box.insertAdjacentHTML("beforebegin", "Hello")
+  DOMSelectors.box.insertAdjacentHTML(
+    "beforebegin",
+    `We can add text into an HTML file by writing it in JS!`
+  );
+ 
+ 
+  DOMSelectors.button.addEventListener("click", function () {
+    let input1 = DOMSelectors.input1.value;
+    let input2 = DOMSelectors.input2.value;
+    DOMSelectors.box.insertAdjacentHTML(
+      "afterend",
+      `
+      <div class="child">
+          <h2 class="text" id="card">${input1}</h2>
+          <h3 class="text" id="card">${input2}</h3>
+          <button type="submit" id="delete">Delete</button>
+      </div>
+      `
+    );
+  });
+
